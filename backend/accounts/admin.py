8f8b390353b37846f5a255d51e8a8a1a5c99bb70
Islam412 <islam.124.hamdy@gmail.com>
2026-07-03 20:28:02 +0300

    desing user profile admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from userauths.models import User, Profile
 
 # Register your models here.
+
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_filter = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    filter_horizontal = ('user_permissions',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'email', 'username', 'address', 'phone', 'verified']
+    list_editable = ['verified']
+    search_fields = ['user__username', 'phone', 'address']
+    list_filter = ['verified']
+    ordering = ['user']
+
+
+admin.site.register(User, UserCustomAdmin)
+admin.site.register(Profile, ProfileAdmin)
