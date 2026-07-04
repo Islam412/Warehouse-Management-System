6d7e7026174b3d5cd950d48b04912ef800159cec
Islam412 <islam.124.hamdy@gmail.com>
2026-07-04 21:58:31 +0300

    add (env.local, next.config.ts, tailwind.config.ts)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', '127.0.0.1'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**',
+      },
+    ],
+  },
+  // دعم RTL
+  i18n: {
+    locales: ['ar', 'en'],
+    defaultLocale: 'ar',
+    localeDetection: true,
+  },
 };
 
 export default nextConfig;
